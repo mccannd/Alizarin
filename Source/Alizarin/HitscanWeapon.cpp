9415ef31b91fd90c614eb4a27bec61a56cf3b56b
mccannd <mccannd@seas.upenn.edu>
2016-05-19 20:43:49 -0700

    continued work on hitscan weapon

--- Source/Alizarin/HitscanWeapon.cpp
+++ Source/Alizarin/HitscanWeapon.cpp
@@ -3,18 +3,83 @@
 #include "SpaceDiablo.h"
 #include "HitscanWeapon.h"
 
+// gets a random direction with a bias towards the center
+FVector getRandomSpreadDirection(float spreadHalfArc, FVector& direction, bool biased = false)
+{
+	// if it's not biased just make a uniformly random rotation
+	if (!biased) return direction.RotateAngleAxis(FMath::FRandRange(-spreadHalfArc, spreadHalfArc), FVector(0, 0, 1));
 
+	// get random (not uniform) point in circle
+	float randomArc = FMath::FRandRange(0, 360);
+	FVector randomRad = FVector(FMath::FRandRange(0, spreadHalfArc) / 90.0f, 0, 0);
+	FVector randomCirclePoint = randomRad.RotateAngleAxis(randomArc, FVector(0, 0, 1));
+	// project point from circle onto line, deviation from 0 x coordinate is angle
+	float biasedAngle = randomCirclePoint.X * 90.0f;
+	return direction.RotateAngleAxis(biasedAngle, FVector(0, 0, 1));
+}
+
+FVector getTraceEnd(FVector start, FVector dir, float dist, float spread = 0.0f)
+{
+	if (FMath::IsNearlyZero(spread)) return start + dist * dir;
+	else return start + dist * getRandomSpreadDirection(spread, dir);
+}
+
+// TODO: muzzle flash sprite
+// TODO: beam along trace
+// TODO: impact sprite
+// TODO: sounds
+void AHitscanWeapon::fireSinglePellet(
+	FVector origin,
+	FVector direction, 
+	ECollisionChannel& channel,
+	FCollisionQueryParams& params)
+{
+	// get line trace endpoint for this pellet
+	FVector end = getTraceEnd(origin, direction, range, spreadHalfArc);
+
+	if (!piercing) {
+		// if the weapon is single target, do a single trace
+		FHitResult collision = FHitResult(ForceInit);
+		GetWorld()->LineTraceSingle(collision, origin, end, channel, params);
+
+		// inflict damage if collided with damageable actor
+		if (collision.GetActor() != NULL) {
+			if (collision.Actor->GetClass()->ImplementsInterface(UDamageable::StaticClass())) {
+				IDamageable* obj = InterfaceCast<IDamageable>(collision.GetActor());
+				obj->TakeDamage(MakeDamage());
+			}
+		}
+	}
+	else {
+		// otherwise do a multiline trace
+		TArray<FHitResult> collisions;
+		GetWorld()->LineTraceMulti(collisions, origin, end, channel, params);
+
+		// TODO: make obstructions that interrupt trace since results are sorted
+		for (FHitResult col : collisions) {
+			if (col.GetActor() != NULL) {
+				if (col.GetActor()->GetClass()->ImplementsInterface(UDamageable::StaticClass())) {
+					IDamageable* obj = InterfaceCast<IDamageable>(col.GetActor());	
+					obj->TakeDamage(MakeDamage());
+				}
+			}
+		}
+	}
+}
+
+// TODO: delay for burst fire weapons
 void AHitscanWeapon::FireHold(FVector origin, FVector direction)
 {
 	if (!CanFireNextShot()) return;
-	
-	// if the weapon is single target, do a single trace
-	// inflict damage if collided with damageable actor
-	
+	// initialize parameters for line trace
+	ECollisionChannel channel = ECC_WorldDynamic;
+	FCollisionQueryParams params(false);
 
-	// otherwise do a multiline trace
-	// inflict damage to all damageable actors
-	
+	for (int pellet = 0; pellet < numPellets; pellet++) {
+		// TODO: create timer to simulate burst fire
+		fireSinglePellet(origin, direction, channel, params);
+	}
+
 	// set the next shot timer
 	nextShotTime = GetWorld()->TimeSeconds + shotDelay;
 }
--- Source/Alizarin/HitscanWeapon.h
+++ Source/Alizarin/HitscanWeapon.h
@@ -15,10 +15,31 @@ class ALIZARIN_API AHitscanWeapon : public AWeapon
 	
 	
 public:
-
+	// function called when left mouse button held down
 	UFUNCTION(BlueprintCallable, Category = "Weapon")
 		virtual void FireHold(FVector origin, FVector direction) override;
 
+	// if false, stops at first collision. if true, pierces all in path
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Any)
-		bool singleTarget = true;
+		bool piercing = false;
+
+	// number of shots fired in a single trigger pull, can create burst fire or shotgun
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Any)
+		int32 numPellets = 1;
+
+	// inaccuracy of the random, zero by default
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Any)
+		float spreadHalfArc = 0.0f;
+
+	// editable range of the hitscan weapon
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Any)
+		float range = 1500.0f;
+
+	// called by FireHold for each pellet fired, handles line tracing
+	void fireSinglePellet(
+		FVector origin,
+		FVector direction,
+		ECollisionChannel& channel,
+		FCollisionQueryParams& params
+		);
 };
